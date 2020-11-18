3abd7a6f70e0b91edcc2c047b9fc456cbdac3b9c
Batuhan Duzgun <batuhan.duzgun@sahibinden.com>
2020-11-18 23:03:18 +0300

    rectangle node module!

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-var rect = {
-    perimeter: (x,y) => (2*(x + y)),
-    area: (x,y) => (x*y)
-};
+var rect = require('./modules/rectangle');
 
 function solveRect(l,b) {
 
     if(l <= 0 || b <= 0) {
-        console.log("l or b can not be zero! l=" + l + ", b=" + b);
+        console.log('l or b can not be zero! l=' + l + ', b=' + b);
     }
     else {
-        console.log("The area of rectangle: " + rect.area(l,b));
-        console.log("The perimeter of rectangle: " + rect.perimeter(l,b));
+        console.log('The area of rectangle: ' + rect.area(l,b));
+        console.log('The perimeter of rectangle: ' + rect.perimeter(l,b));
     }
 }
 
