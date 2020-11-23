36ce0407a08f39506704b2aa4333c7cdec4b2902
Batuhan Duzgun <batuhan.duzgun@sahibinden.com>
2020-11-23 10:13:36 +0300

    node http server html serving!

--- node-http/index.js
+++ node-http/index.js
@@ -1,16 +1,52 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 80;
 
+var notFoundPage = (res) => {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>File not found!</h1></body></html>');
+};
 
 const server = http.createServer( (req, res) => {
 
-    console.log(req.headers);
+    console.log('Request for ' + req.url + ' by method ' + req.method);
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello Node JS!</h1></body></html>');
+    if(req.method == 'GET') {
+
+        var fileUrl = '';
+        if(req.url == '/') {
+            fileUrl = '/index.html';
+        }
+        else {
+            fileUrl = req.url;
+        }
+
+        var filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+        if(fileExt == '.html') {
+
+            fs.exists(filePath, (exists) => {
+                if(!exists) {
+                    notFoundPage(res);
+                    return;
+                }
+
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+
+                fs.createReadStream(filePath).pipe(res);
+            });
+        }
+        else {
+            notFoundPage(res);
+            return;
+        }
+    }
+
 } );
 
 server.on('listening', () => {
